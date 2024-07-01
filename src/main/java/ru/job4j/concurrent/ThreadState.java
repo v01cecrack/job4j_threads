36718b3604b448a809fd001df7b19ed4853b3994
Kostya <k.kharionovsky@cloudcom.ru>
2024-07-01 13:37:07 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        first.start();
+        second.start();
+        try {
+            first.join();
+            second.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Работа завершена");
+
+    }
+
+}
