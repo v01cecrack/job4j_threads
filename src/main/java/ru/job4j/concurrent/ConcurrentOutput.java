cdb41b6d9e37a50a3e1b4a7ea736275224c08a8a
Kostya <k.kharionovsky@cloudcom.ru>
2024-07-01 11:58:59 +0300

    2. Создание и запуск нити [#1016]

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,11 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        System.out.println(Thread.currentThread().getName());
+        another.start();
+        second.start();
+    }
+}
