6254b9b689e1f3a3a82842eaca135353b4fea8b7
Kostya <k.kharionovsky@cloudcom.ru>
2024-07-02 10:52:48 +0300

    4. Режим ожидания. [#231217 #503161]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,9 +1,13 @@
 package ru.job4j.concurrent;
 
 public class Wget {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            Thread.sleep(100);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.print("\rLoading : " + i  + "%");
         }
     }
