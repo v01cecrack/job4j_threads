4b738cdc9ec16ecc5b7ef6c8156255275dc0ddb0
Kostya <k.kharionovsky@cloudcom.ru>
2024-07-02 09:33:46 +0300

    2. Создание и запуск нити [#1016 #503121]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,8 +2,12 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
-        Thread another = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
         System.out.println(Thread.currentThread().getName());
         another.start();
         second.start();
